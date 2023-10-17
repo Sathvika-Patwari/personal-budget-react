a92086af5e4c5db14dede691793cd76e3dabb76c
Sathvika Patwari <sathvika0123@gmail.com>
2023-10-16 20:51:50 -0400

    input data path correction in server

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3004;
-const BudgetDataJSON = require('./budgetData.json');
+const BudgetDataJSON = require('../public/budgetData.json');
 
 app.use('/heyo',express.static('public'));
 
